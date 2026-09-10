38f6763305ac205f8a9a010fc54ff42daa511c95
Tannequa <whitehet6381@student.faytechcc.edu>
2026-09-10 11:20:28 -0400

    P1LAB

---
+++ P1LAB1_WhiteheadTannequa.py
@@ -0,0 +1,16 @@
+# Tannequa Whitehead
+# 09/10/2026
+# Inputs and Outputs
+
+# Get first name
+first_name = input("Enter your first name: ")
+
+# Get last name
+last_name = input("Enter your last name: ")
+
+# Output welcome message to user
+# Use concatenation in the output (+)
+print()
+print()
+# Use f-string
+print(f"Hello, {first_name} {last_name}!!! Welcome to CTI-110")
